3e9fa8c84652ebe76d5efc196085d5ba56d62f4a
GromoZone <zakgromozone@mail.ru>
2021-04-14 23:04:26 +0300

    gchnjcfjh

---
+++ app/src/main/java/com/example/home_work/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.home_work;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_homework1_1);
+        setContentView(R.layout.activity_homework1_2);
+    }
+}
